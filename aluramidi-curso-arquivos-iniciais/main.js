1d07caea1e40bd495aa79883951a26acc59ec79f
AnnaDays <analuizadias389@gmail.com>
2024-07-12 12:49:45 -0300

    Projeto Alura Midi - Atualizado

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,11 @@
-function tocaSom (idTagAudio){
-    document.querySelector(idTagAudio).play() //play -> reproduz o som do audio na página
+function tocaSom (seletorAudio){
+   const elemento = document.querySelector(seletorAudio); //play -> reproduz o som do audio na página
+
+   if(elemento && elemento.localName === 'audio'){
+     elemento.play();
+   } else {
+     alert('Elemento NÃO ENCONTRADO ou Seletor INVÁLIDO!!!')
+   }
 }
  
 const listadeTeclas = document.querySelectorAll('.tecla');
@@ -15,5 +21,16 @@ for (let contador = 0; contador < listadeTeclas.length; contador++){
 
     tecla.onclick = function () { // para essa função ser chamada, só é possivel se o usuário clicar no botão
         tocaSom(idAudio); //passando a constante IdAudio como parâmetro pra função tocaSom
+    }
+
+    //onkeydown -> função de quando a tecla ou botão é clicado/abaixado
+    tecla.onkeydown = function (evento){ //pode usar (event) ou (e)
+        if (evento.code === 'Enter' || evento.code === 'Space'){ //verifica se a tecla/evento foi acionado pela tecla Enter ou Espaço
+          tecla.classList.add('ativa'); //adiciona uma classe chamada ativa para ativar a cor vermelha no botão, clicando enter e espaço
+        }
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
     }
 }
